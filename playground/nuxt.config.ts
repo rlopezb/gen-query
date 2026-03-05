d223751ba9261ba046fb0d70fc75d951485f975b
Rafael López Benavente <rlopezb@gmail.com>
2026-03-05 20:12:18 +0100

    fix: add missing sourcemap configuration in Nuxt config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
+  sourcemap: {
+    server: true,
+    client: true,
+  },
   compatibilityDate: '2024-04-03',
   genQuery: {
     baseURL: 'https://oracloud.mooo.com:8443',
